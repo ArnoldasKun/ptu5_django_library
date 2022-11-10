76ea0c0cfae68fc4b10c32ffe344c9cb8c94e0d8
Arnoldas Kundrotas <arnokund@gmail.com>
2022-11-10 08:51:41 +0200

    add 4 pamoka ir komentarai

--- ptu5_library/library/views.py
+++ ptu5_library/library/views.py
@@ -8,18 +8,29 @@ def index(request):
 
     # views gali buti 2 tipu funkciniai ir class based
     #funkciniai visada reikalauja request
-    book_count = Book.objects.count()
-    book_instances_count = Book.objects.count()
-    book_instances_available_count = BookInstance.objects.filter(status='a').count()
-    author_count = Author.objects.count()
 
+    #Susirenkam duomenis is models.py
+    # 4 skaitliukai
+    book_count = Book.objects.count()#kiek knygu
+    book_instances_count = Book.objects.count()#kiek kopiju
+    #jeigu kintamasis iki 6 zodziu, gali buti aprasytas, paskui trumpinamas
+    book_instances_available_count = BookInstance.objects.filter(status='a').count()#kiek avalaible knygu
+    # status = 'a' - available
+    author_count = Author.objects.count()#kiek autoriu
+
+    #context - obj, kuri pasiduoda i templates
     context = {
-        'book_count': book_count,
+        'book_count': book_count, #ivadas: reiksme
         'book_instances_count': book_instances_count,
         'book_instances_available_count': book_instances_available_count,
         'author_count': author_count,
-        'genre_count': Genre.objects.count()
+        'genre_count': Genre.objects.count()#zanrus pasiduodam cia, tokiu budu
 
     }
 
     return render(request, 'library/index.html', context)
+    #return render - nupies template
+    #gautas request
+    #template pavadinimas, savo apps katalogo pavadinimas,
+        #irasomas tas, kuris pirmas yra settings.py/installed_apps
+    #context - visada trecias argumentas
